4be2d316c5af6ab15259d674bd5ac248cfc051bb
congnv <congnv@etc.vn>
2024-02-22 14:54:40 +0700

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [   
+    TypeOrmModule.forRoot()
+  ]
 })
 export class AppModule {}
